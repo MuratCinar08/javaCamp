ff2502ad64534e53713da277945be9d5dc2ab5fa
Murat Çınar <murat_cinar_bjk@hotmail.com>
2022-09-16 18:21:44 +0300

    First commit

---
+++ arraysDemo.java
@@ -0,0 +1,36 @@
+public class arraysDemo {
+
+    public static void main (String[] args) {
+        String ogrenci1 = "Murat";
+        String ogrenci2 = "Ahmet";
+        String ogrenci3 = "Sıla";
+        String ogrenci4 = "Ezgi";
+
+        System.out.println(ogrenci1);
+        System.out.println(ogrenci2);
+        System.out.println(ogrenci3);
+        System.out.println(ogrenci4);
+
+        System.out.println("----------------");
+
+        String[] ogrenciler = new String[3];
+        ogrenciler[0] = "Murat";
+        ogrenciler[1] = "Sıla";
+        ogrenciler[2] = "Ezgi";
+
+        for(int i=0;i<ogrenciler.length;i++) {
+            System.out.println(ogrenciler[i]);
+
+        }
+        System.out.println("-----------------");
+        for(String ogrenci: ogrenciler) {
+            System.out.println(ogrenci);
+        }
+
+    
+
+
+
+    }
+    
+}
