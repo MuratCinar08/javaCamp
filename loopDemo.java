9b73f9e4b2f9bdfb68f4f040da9686abbdf212e0
Murat Çınar <murat_cinar_bjk@hotmail.com>
2022-09-16 17:36:19 +0300

    Firs Commit

---
+++ loopDemo.java
@@ -0,0 +1,12 @@
+public class loopDemo {
+
+    public static void main (String[] args) {
+
+       for(int i=1;i<=10;i++) {
+        System.out.println(i);
+       }
+        System.out.println("Döngü Bitti");
+
+    }
+    
+}
