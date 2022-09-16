a2dcf0a3179d5d215d40f5451208697a14d85070
Murat Çınar <murat_cinar_bjk@hotmail.com>
2022-09-16 17:54:25 +0300

    First Commğit

--- loopDemo.java
+++ loopDemo.java
@@ -2,10 +2,33 @@ public class loopDemo {
 
     public static void main (String[] args) {
 
-       for(int i=1;i<=10;i++) {
+       //For
+       for(int i=2;i<=10;i+=2) {
         System.out.println(i);
        }
-        System.out.println("Döngü Bitti");
+        System.out.println("For Döngüsü Bitti");
+        int i = 1;
+
+       //While
+       while(i<10) {
+        System.out.println(i);
+        i++;
+       }
+        System.out.println("While Döngüsü Bitti");
+       //Do-While
+       int j = 100;
+       do{
+         System.out.println(j);
+         j+=2;
+       }while(j<10);
+        System.out.println("Do-While Döngüsü Bitti"); 
+
+
+
+
+       
+
+
 
     }
     
