4a015c5dad07c0efd96fc0cc299a124878571086
Murat Çınar <murat_cinar_bjk@hotmail.com>
2022-09-17 15:18:37 +0300

    First Commit

---
+++ miniProjeAsalSayi.java
@@ -0,0 +1,41 @@
+public class miniProjeAsalSayi {
+
+    public static void main (String[] args) {
+
+        int number = 35 ;
+        int remainder = number % 2;
+        System.out.println(remainder);
+        boolean isPrime = true;
+
+        if(number==1){
+            System.out.println("Sayı Asal Değildir");
+            return;
+        }
+
+        if(number<1) {
+            System.out.println("Geçersiz Sayı");
+        }
+
+        for(int i= 2; i<number; i++){
+        if(number % i == 0){
+            isPrime = false;
+
+        }
+        }
+        if(isPrime) {
+            System.out.println("Sayı Asaldır");
+
+        }else{
+            System.out.println("Sayı Asal Değildir");
+        }
+
+
+    
+    
+    
+    
+    
+    
+    }
+    
+}
