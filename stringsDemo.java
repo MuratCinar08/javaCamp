e29f889dfa46fe603ee2a582b24375e1b83ef21a
Murat Çınar <murat_cinar_bjk@hotmail.com>
2022-09-17 14:26:28 +0300

    First Commit

--- stringsDemo.java
+++ stringsDemo.java
@@ -2,19 +2,47 @@ public class stringsDemo {
 
     public static void main (String [] args) {
 
-        String mesaj = "Bugün hava çok yağmurlu";
+        String mesaj = "   Bugün hava çok yağmurlu   ";
+
         System.out.println(mesaj);
 
         System.out.println("Eleman Sayısı: " + mesaj.length());
+
         System.out.println("5. Eleman: " + mesaj.charAt(4));
+
         System.out.println(mesaj.concat(" Şemsiye Alalım"));
+
         System.out.println(mesaj.startsWith("B"));
+
         System.out.println(mesaj.endsWith("."));
+
         char[] karakterler = new char[5];
+
         mesaj.getChars(0, 5, karakterler, 0);
+
         System.out.println(karakterler);
+
         System.out.println(mesaj.indexOf("av"));
+
         System.out.println(mesaj.lastIndexOf("a"));
+
+        String yeniMesaj = mesaj.replace(' ', '-');
+
+        System.out.println(yeniMesaj);
+
+        System.out.println(mesaj.substring(2, 5));
+
+        for(String kelime : mesaj.split (" ") ) {
+            System.out.println(kelime);
+        }
+
+        System.out.println(mesaj.toLowerCase());
+        System.out.println(mesaj.toUpperCase());
+        System.out.println(mesaj.trim());
+        
+
+
+
     }
 
     
