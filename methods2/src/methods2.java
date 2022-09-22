fc963c53455ca5ad2d9e1ad96c41d69bd4f04090
Murat Çınar <murat_cinar_bjk@hotmail.com>
2022-09-22 15:36:52 +0300

    First Commit

--- methods2/src/methods2.java
+++ methods2/src/methods2.java
@@ -5,6 +5,8 @@ public class methods2 {
        System.out.println(newMessage);
        int number = addition(15, 27);
        System.out.println(number);
+       int total = addition2(2,4,6,8,10);
+       System.out.println(total);
     }
 
     public static void add() {
@@ -25,5 +27,14 @@ public class methods2 {
 
     public static int addition(int num1, int num2){
         return num1+num2;
+    }
+
+    public static int addition2(int... numbers) {
+        int total = 0;
+        for(int number:numbers) {
+            total+=number;
+            
+        }
+        return total;
     }
 }
