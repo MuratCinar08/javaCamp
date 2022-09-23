3246efb3eeb6751f25c0416cebd90bbdac1afad1
Murat Çınar <murat_cinar_bjk@hotmail.com>
2022-09-23 10:54:40 +0300

    First Commit

---
+++ methodOverloading/src/FourOperations.java
@@ -0,0 +1,9 @@
+public class FourOperations {
+    public int Add(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public int Add(int num1, int num2, int num3) {
+        return num1 + num2 + num3;
+    }
+}
---
+++ methodOverloading/src/methodOverloading.java
@@ -0,0 +1,7 @@
+public class methodOverloading {
+    public static void main(String[] args) {
+        FourOperations fourOperations = new FourOperations();
+        System.out.println(fourOperations.Add(97, 88));
+        fourOperations.Add(5, 25, 475);
+    }
+}
